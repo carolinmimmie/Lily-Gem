c6ce1349dca621f0c4b2dfb6c641d35cb0e8412f
carolinmimmie <carolinmimmie@gmail.com>
2025-09-12 10:54:18 +0200

    test

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -4,16 +4,13 @@ import { Product } from "@/types/product";
 import { groq } from "next-sanity";
 import React from "react";
 
-interface ProductPageProps {
-  params: {
-    slug: string;
-  };
-}
-
-// Viktigt: lägg till "async function" istället för arrow function
-export default async function ProductPage({ params }: ProductPageProps) {
+// Async server component
+export default async function ProductPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const query = groq`*[_type == "product" && slug.current == $slug][0]`;
-
   const product: Product = await client.fetch(query, { slug: params.slug });
 
   return (
